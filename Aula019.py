7aa92e41cd9ce6a851f6a1936061391853efc804
Lazaro-Costta <lasarocosta11@gmail.com>
2025-05-29 16:06:26 -0300

    Update Aula019.py

--- Aula019.py
+++ Aula019.py
@@ -1,2 +1,10 @@
-pessoa = {'nome': 'lasaro', 'idade': '24', 'sexo': 'masculino'}
-print(pessoa['nome'])
+#pessoa = {'nome': 'lasaro', 'idade': '24', 'sexo': 'masculino'}
+#print(f'{pessoa["nome"]} tem {pessoa["idade"]} anos')
+#for c, p in pessoa.items():
+    #print(f'{c} = {p}')
+brasil = []
+estado1 = {'uf': 'maranhão', 'situação': 'pobreza extrema'}
+estado2 = {'uf': 'balneario', 'situação': 'rico'}
+brasil.append(estado1)
+brasil.append(estado2)
+print(f'{brasil[0] ["uf"]} tem uma {brasil[0] ["situação"]}')
